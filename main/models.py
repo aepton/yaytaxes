ad186b43bf6bdd2c87cdc2d3293f6829547894cb
aepton <abraham.epton@gmail.com>
2011-06-25 15:30:06 -0700

    Finished implementing tweets (still need to test...)

--- main/models.py
+++ main/models.py
@@ -6,3 +6,19 @@ class HouseholdIncome(models.Model):
 	
 	def __unicode__(self): return '%s (%s) (%s)' % (self.id, self.income, self.households)
 
+class Tweets(models.Model):
+  user = models.CharField(max_length=50)
+  tweet_text = models.CharField(max_length=140)
+  service = models.CharField(max_length=50)
+  budget_amount = models.IntegerField()
+  
+  def __unicode__(self): return '%s (%s) (%s) (%s) (%s)' % (self.id,
+                                                            self.user,
+                                                            self.tweet_text,
+                                                            self.service,
+                                                            self.budget_amount)
+  
+class HoodAttributes(models.Model):
+  attributes = models.TextField()
+  
+  def __unicode__(self): return '%s (%s)' % (self.id, self.attributes)
--- main/views.py
+++ main/views.py
@@ -1,28 +1,42 @@
 from django.template import Context, loader
 from django.http import HttpResponse
 from main.models import HouseholdIncome
+from main.models import HoodAttributes
+from main.models import Tweets
 import json
+import random
 
 def index(request):
 	t = loader.get_template('main/index.html')
 	c = Context()
 	return HttpResponse(t.render(c))
+	
+def scale(raw):
+  return 20 * int(round(float(raw)/float(20)))
 
 def tweets(request):
   user_id = request.GET.get('id', '')
   if not user_id:
     print 'No ID provided'
     return HttpResponse('No ID provided', status=500)
-    
-  # First, get all decisions made by user
+  
+  # First, get all user neighborhood attributes
+  neighborhood = json.loads(HoodAttributes.objects.get(id=user_id).attributes)
+  print 'Atts: %s' % neighborhood
   
   # Next, get all relevant tweets for those decisions
-  
+  dimensions = ['environment', 'government', 'infrastructure', 'safety', 'k-12',
+                'higher ed']
+  candidates = []
+  for d in dimensions:
+    tweets = Tweets.objects.filter(budget_amount=scale(neighborhood[d]),
+                                   service=d)
+    for tweet in tweets:
+      candidates.append({'tweet_text': tweet.tweet_text, 'user': tweet.user})
+    
   # Finally, decide which to display
-  # Importantly, don't want to display the same one twice consecutively
-  
-  response_obj = {'tweet_text': 'I am a tweet', 'user': 'aepton'}
-  return HttpResponse(json.dumps(response_obj))
+  index = random.randint(0, len(candidates) - 1)
+  return HttpResponse(json.dumps(candidates[index]))
 	
 def yaytaxesland(request):
 	t = loader.get_template('main/yaytaxesland.html')
